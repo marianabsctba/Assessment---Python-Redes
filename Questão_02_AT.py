adbbb5c02f86a7cb4a8b072fd29a098d2fcd70ec
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2021-03-16 19:24:57 -0300

    Create Questão_02_AT.py

---
+++ Questão_02_AT.py
@@ -0,0 +1,28 @@
+#Questão 2)
+#Escreva um programa que obtenha um nome de um arquivo texto do usuário e crie um processo para
+#executar o programa do sistema Windows bloco de notas (notepad) para abrir o arquivo.
+
+import os, platform, subprocess
+
+
+user_file = (input("Qual arquivo de texto você deseja abrir? ") + ".txt")
+
+    
+def open_file_notepad(user_file):
+    if os.path.isfile(user_file):
+        
+        platformer = platform.system()
+        
+        if platformer == "Windows":
+            print("Sistema Operacional: ", platformer)
+            os.system(f"notepad {user_file}")
+        
+        else:
+            print("Sistema Operacional: ", platformer)
+            subprocess.Popen(['APP_COMMAND', f'~/{user_file}'])      
+    else:
+        print(user_file, 'não é um arquivo. Tente novamente')
+        exit(1)
+        
+        
+open_file_notepad(user_file)
