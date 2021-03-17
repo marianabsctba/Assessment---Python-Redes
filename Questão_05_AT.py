e281b2599636def16066d76ee492c7f07e1241d8
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2021-03-17 16:41:36 -0300

    Update Questão_05_AT.py

--- Questão_05_AT.py
+++ Questão_05_AT.py
@@ -4,3 +4,32 @@
 #Seu programa deve somar elemento por elemento de cada arquivo e imprimir o resultado na tela.
 #Isto é, o primeiro elemento de a.txt deve ser somado ao primeiro elemento de b.txt, segundo elemento de a.txt deve ser somado ao segundo elemento de b.txt,
 #e assim sucessivamente. Caso um arquivo tenha mais elementos que o outro, os elementos que sobrarem do maior devem ser somados a zero.
+
+
+def lining():
+    print("==" * 40)
+
+def sum_data_files(): 
+
+    lines, cols = 1, 10      
+    sums = [[0] * cols for _ in range(lines)]
+
+    files_names = ["a.txt", "b.txt"]
+    
+    for file in files_names:
+        with open(file) as f:
+            for i, lin in enumerate(f):
+                for j, col in enumerate(lin.split()):
+                    sums[i][j] += int(col)
+                    
+                    
+    lining()
+    
+    print("O resultado da soma dos números dos arquivos", files_names[0], "e", files_names[1], "é: ")
+    
+    for lin in sums:
+        print(*lin)
+    lining()
+
+
+sum_data_files()
