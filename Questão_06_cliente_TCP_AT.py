f6d4d2c6bfc95fe4ddd3453518b1e53d004f5478
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2021-03-29 14:32:56 -0300

    Update Questão_06_cliente_TCP_AT.py

--- Questão_06_cliente_TCP_AT.py
+++ Questão_06_cliente_TCP_AT.py
@@ -3,34 +3,39 @@
 #O cliente envia para o servidor o nome de um diretório e recebe a lista de arquivos (apenas arquivos) existente nele.
 #O servidor recebe a requisição do cliente, captura o nome dos arquivos no diretório em questão e envia a resposta ao cliente de volta.
 
-import pickle
-import socket
-#client
+import socket, pickle
 
-def Print_Status(bytes, tam):
-    kb = bytes/1024
-    tam_bytes = tam/1024
-    txt = 'Baixando... '
-    txt = txt + '{:<.2f}'.format(kb) + ' KB '
-    txt = txt + 'de ' + '{:<.2f}'.format(tam_bytes) + ' KB'
-    print(txt)
+#SOCKET CLIENTE
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 5000))
-# Tenta se conectar ao servidor
+def client():    
+    #socket cliente
 
-choice = ''
-while not choice == 's':
-    choice = input('choice um diretório: ')
-    # Envia a requisição:
-    s.send(choice.encode('ascii'))
-    # Recebe o tamanho do arquivo:
-    msg = s.recv(65535)
-    res = pickle.loads(msg)
-    for i in res:
-        print(i)
-    choice = ''
-    print("Digite 's' para sair")
+    HOST = socket.gethostname()
+    PORT = 9999
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+    
+    diret = input('Qual o nome do diretório? ')
+    s.send(diret.encode('ascii'))
+
+    request = b''
+    
+    while True:   # faz o controle dos dados recebidos do servidor (converte espaços vazios)    
+        data = s.recv(1024);
+        print('Dados recebidos do servidor...')        
+        if not data: break
+        request = request + data                
+    
+    data = pickle.loads(request)
     s.close()
+   
+    if data == list(data):
+        for i in data:
+            print(i)
+    else:
+        print(data)
     
-    
+        
+
+if __name__ == '__main__':
+    client()
