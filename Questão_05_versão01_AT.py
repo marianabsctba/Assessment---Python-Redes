dd04d7e45eb9ef387186367c527010f2483ffb1a
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2021-03-30 11:21:03 -0300

    Update Questão_05_versão01_AT.py

--- Questão_05_versão01_AT.py
+++ Questão_05_versão01_AT.py
@@ -5,14 +5,13 @@
 #Isto é, o primeiro elemento de a.txt deve ser somado ao primeiro elemento de b.txt, segundo elemento de a.txt deve ser somado ao segundo elemento de b.txt,
 #e assim sucessivamente. Caso um arquivo tenha mais elementos que o outro, os elementos que sobrarem do maior devem ser somados a zero.
 
-
 import numpy as np
 
 def files(file_name):
     try:
         with open(file_name, 'r') as file:
             data = file.read().split()
-        return data
+        return list(data)
     except FileNotFoundError as error:
         print(str(error))
         return []
@@ -24,11 +23,23 @@ def lis(first_list):
         try:
             second_list.append(int(v))
         except ValueError:
-            print(v, 'não é umm número válido.')
+            print(v, 'não é um número válido.')
             second_list.append(0)
     return second_list
 
-        
+
+def sum_files():   
+    count_a = len(a)
+    count_b = len(b)
+
+    if count_a < count_b:
+        a[count_a:count_b] = (count_b - count_a) * [0]
+    else:
+        b[count_b:count_a] = (count_a - count_b) * [0]
+
+    return (np.array(a) + np.array(b)) 
+
+
 a = files('a.txt')
 b = files('b.txt')
 
@@ -38,12 +49,4 @@ print('B:', b)
 a = lis(a)
 b = lis(b)
 
-count_a = len(a)
-count_b = len(b)
-
-if count_a < count_b:
-    a[count_a:count_b] = (count_b - count_a) * [0]
-else:
-    b[count_b:count_a] = (count_a - count_b) * [0]
-
-print('Soma:', np.array(a) + np.array(b))
+print("Soma: ", sum_files()))
