bf32c4d2a0545ab5ad71f096eb472596bcd8a4cd
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2021-03-30 00:05:15 -0300

    Update Questão_04.AT.py

--- Questão_04.AT.py
+++ Questão_04.AT.py
@@ -3,25 +3,30 @@
 
 import os
 
-
+#file_name = (input("Digite o nome do arquivo: ") + '.txt')
 file_name = "questao4.txt"
 
-
 def reversed_file(file_name):
-    with open(file_name) as file:
-        file.seek(0, os.SEEK_END)
-        pos = file.tell()
-        line = ''
-        while pos >= 0:
-            file.seek(pos)
-            next_line = file.read(1)
-            if next_line == "\n":
-                yield line[::-1]
-                line = ''
-            else:
-                line += next_line
-            pos -= 1
-        yield line[::-1]
+    try:
+        with open(file_name) as file:
+            
+            file.seek(0, os.SEEK_END)
+            pos = file.tell()
+            line = ''
+            while pos >= 0:
+                file.seek(pos)
+                next_line = file.read(1)
+                
+                if next_line == "\n":
+                    yield line[::-1]
+                    line = ''
+                else:
+                    line += next_line
+                    
+                pos -= 1
+            yield line[::-1]
+    except FileNotFoundError as error:
+        print(error)
 
 
 if __name__ == '__main__':
