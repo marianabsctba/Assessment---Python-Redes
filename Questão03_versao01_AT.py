aedc17921284cd920334c45dde3c08d57530810a
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2021-03-30 13:43:10 -0300

    Create Questão03_versao01_AT.py

---
+++ Questão03_versao01_AT.py
@@ -0,0 +1,42 @@
+import os
+
+    
+directory = input("Digite o diretório: ") #versão contendo input do usuário
+
+def see_files():
+    if os.path.exists(directory):
+        pairs = []
+        lists = os.listdir(directory)
+        
+        for file in lists:
+            location = os.path.join(directory, file)        
+            if os.path.isfile(location):
+                
+                size = os.path.getsize(location)
+                size = round((size/1024), 2)
+                pairs.append((location, size))
+
+        pairs.sort(key=lambda s: s[1], reverse=True) #do maior tamanho para o menor
+        return pairs
+    else:
+        print("O diretório não existe. Tente novamente.")
+        exit()
+
+        
+def print_files():    
+    text = ''
+    
+    p = see_files()
+    
+    for l, pair in p:
+        text += "Arquivo: {:>20}, tamanho(kB): {:<10}\n".format(l, pair)
+        #print(text)
+
+    new_file = open("questao3_AT.txt", "w")
+    new_file.write(text)
+    new_file.close()
+    os.startfile('questao3_AT.txt')
+    
+
+see_files()
+print_files()
