8cae11ff86bdcd6e7e736cc00100c993f5385a04
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2021-03-30 14:17:24 -0300

    Create Questão_02_versao02_AT.py

---
+++ Questão_02_versao02_AT.py
@@ -0,0 +1,20 @@
+import os, platform, subprocess
+
+
+user_file = (input("Qual arquivo de texto você deseja abrir? ") + ".txt") #aqui eu limitei a abertura de arquivos txt de modo genérico, só para testar
+
+platformer = platform.system()
+    
+def open_file_notepad(): 
+    try:    
+        if platform.system() == 'Darwin':       #MACOS
+            subprocess.call(('open', user_file))
+        elif platform.system() == 'Windows':    #windows
+            os.startfile(user_file)
+        else:                                   
+            subprocess.call(('xdg-open', user_file)) #Linux
+    except FileNotFoundError as e:
+        print(e)
+        
+        
+open_file_notepad()
