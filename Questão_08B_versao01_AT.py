f4a1fee6c29a43c09a276afed35dd0dad859f624
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2021-04-06 14:48:57 -0300

    Update Questão_08B_versao01_AT.py

--- Questão_08B_versao01_AT.py
+++ Questão_08B_versao01_AT.py
@@ -1,15 +1,11 @@
 import threading, time
 from random import randint  #realizando o cálculo comum das threadings
 
-N = 50000
 
-
-A = []
-B = []
-T = []
-
-threads = 4
-
+def lin():
+    print("==" * 30)
+    
+    
 def factorial(n):
     factorial = n
     for i in range(n - 1, 1, -1):
@@ -20,29 +16,43 @@ def factorial(n):
 def calc_fact(lis, start_t, end_t):
     for i in range(start_t, end_t):
         B.append(factorial(lis[i]))
-
-
-def time_execution():
-    start = float(time.time())
-    
-    for i in range(N):
-        A.append(randint(1, 10))
-     
-    for i in range(threads):
-        start_t = i * int(N/threads)
-        end_t = (i + 1) * int(N/threads)
-        th = threading.Thread(target=calc_fact, args=(A, start_t, end_t))
-        th.start()
-        T.append(th)
-    
-    for th in T:
-        th.join()
         
-    end = float(time.time())    
-    elapsed = end - start
 
-    print('Tempo inicial: ', start)
-    print('Tempo final: ', end)
-    print('Tempo total paralelo (4 threads): ', round(elapsed, 2), 'segundos.')
+def print_time():
+    lin()
+    print(f"=== EXECUÇÃO PARALELA : {N} ===")
+    lin()
+    print('Tempo inicial: ', start, 'segundos.')
+    print('Tempo final: ', end, 'segundos.')
+    print('Tempo total: ', elapsed, 'segundos.')
+    lin()
+
+N = 50000
+
+A = []
+B = []
+T = []
+
+threads = 4
+
+start = float(time.time())
+
+for i in range(N):
+    A.append(randint(1, 10))
+ 
+for i in range(threads):
+    start_t = i * int(N/threads)
+    end_t = (i + 1) * int(N/threads)
+    th = threading.Thread(target=calc_fact, args=(A, start_t, end_t))
+    th.start()
+    T.append(th)
+
+for th in T:
+    th.join()
     
-time_execution()
+end = float(time.time())
+
+elapsed = end - start
+    
+
+print_time()
